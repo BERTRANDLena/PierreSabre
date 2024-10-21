89e835de638bd7e7e6651abe7cbe244eae2e9393
Lena Bertrand <brl4376a@ensfsi.onmicrosoft.com>
2024-10-21 18:40:49 +0200

    Création classe Humain

---
+++ src/personnages/Humain.java
@@ -0,0 +1,45 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boisson;
+	private int argent;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public int getArgent() {
+		return argent;
+	}
+
+	public void direBonjour() {
+		System.out.println("Bonjour! Je m'apelle " + nom + " et j'aime boire du " + boisson);
+	}
+
+	public void parler(String texte) {
+		System.out.println(nom + ": <<" + texte + ">>");
+	}
+
+	public void boire() {
+		System.out.println("Mmmm, un bon verre de " + boisson + "! GLOUPS !");
+	}
+
+	public void acheter(String bien, int prix) {
+		if (argent >= prix) {
+			parler("J'ai " + argent + " sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix + " sous");
+			perdreArgent(prix);
+		} else {
+			parler("Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offrir un kimono à " + prix
+					+ " sous.");
+		}
+	}
+
+	public void perdreArgent(int perte) {
+		argent -= perte;
+	}
+
+	public void gagnerArgent(int gain) {
+		argent += gain;
+	}
+}
